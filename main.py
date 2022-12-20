03c33b0b55274942d57bbcfdc24a7201eeaa5f61
Etienne Angé <50795135+EtienneAnge@users.noreply.github.com>
2022-12-20 16:11:31 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -30,11 +30,12 @@ def completion(prompt):
 
 
 def duochat(story_title):
-    filename = "histoires/" + story_title + ".txt"
+    filename = "character/" + story_title + ".txt"
     with open(filename, "r") as f:
       prompt = f.read()
     ai = story_title
-    print("ia :", ai)
+    print("personnage choisi:", ai)
+    print("Début de la conversation: ")
 
     while True:
         input_user = input("\nVous: ")
@@ -45,34 +46,33 @@ def duochat(story_title):
         prompt = prompt + ans
 
 def write_story(story_title, story_text):
-    # Crée un fichier avec le titre de l'histoire comme nom, dans le répertoire 'histoires'
-    filename = "histoires/" + story_title + ".txt"
+    filename = "character/" + story_title + ".txt"
     with open(filename, "w+") as f:
-        # Écrit le titre de l'histoire et le texte dans le fichier
+        # Écrit le nom du personnage et le texte dans le fichier
         f.write(story_title + "\n\n")
         f.write(story_text)
 
 def delete_story(story_title):
-    # Supprime le fichier avec le titre de l'histoire comme nom, dans le répertoire 'histoires'
-    filename = "histoires/" + story_title + ".txt"
+    # Supprime le fichier avec le nom du personnage comme nom, dans le répertoire 'character'
+    filename = "character/" + story_title + ".txt"
     os.remove(filename)
     print("l'histoire: " + filename + "a bien été suprimé")
 
 def retrieve_stories():
-    # Récupère la liste de tous les fichiers dans le répertoire 'histoires'
-    files = os.listdir("histoires/")
+    # Récupère la liste de tous les fichiers dans le répertoire 'character'
+    files = os.listdir("character/")
     # Filtre la liste pour ne garder que les fichiers .txt
     txt_files = [f for f in files if f.endswith(".txt")]
     stories = []
     # Pour chaque fichier .txt, ouvre le fichier et ajoute son contenu à la liste 'stories'
     for txt_file in txt_files:
-        with open("histoires/" + txt_file, "r") as f:
+        with open("character/" + txt_file, "r") as f:
             stories.append(f.read())
             stories.append("\n\n")
     return stories
 
 def read_stories(stories):
-    # Affiche chaque histoire dans la liste
+    # Affiche chaque personnage dans la liste
     for story in stories:
         print(story)
 
@@ -87,47 +87,47 @@ while True:
     choice = input("Choisissez une option: ")
 
     if choice == "1":
-        # Demander le titre et le texte de l'histoire à l'utilisateur
+        # Demander le nom et le detail du personnage à l'utilisateur
         os.system("clear")
         print("Exemple:\nNom du personnage: Harry Potter")
         print("Détail du personnage: Harry Potter est le personnage principale du roman de J. K. Rowling (il est important de situer le personnage)")
         print("Harry Potter vient juste de vaincre Voldemort (n'hesitez pas à donner des instructions au context et a être très précis)")
         story_title = input("\nNom du personnage: ")
         story_text = input("Détail du personnage: ")
-        # Écrire l'histoire
+        # Écrire le personnage
         write_story(story_title, story_text)
 
     elif choice == "2":
         os.system("clear")
-        # Récupère la liste de tous les fichiers dans le répertoire 'histoires'
-        files = os.listdir("histoires/")
+        # Récupère la liste de tous les fichiers dans le répertoire 'character'
+        files = os.listdir("character/")
         # Filtre la liste pour ne garder que les fichiers .txt
         txt_files = [f for f in files if f.endswith(".txt")]
         print(txt_files)
-        # Demander le titre de l'histoire à l'utilisateur
+        # Demander le titre du personnage à l'utilisateur
         story_title = input("Quel personnage voulez-vous supprimer: ")
-        # Supprimer l'histoire
+        # Supprimer le personnage
         delete_story(story_title)
 
     elif choice == "3":
         os.system("clear")
-        # Récupérer toutes les histoires
+        # Récupérer toutes les character
         stories = retrieve_stories()
-        # Afficher toutes les histoires
+        # Afficher toutes les character
         read_stories(stories)
 
     elif choice == "4":
         os.system("clear")
         print("Voici tout les personnes enregirstré:")
-        # Récupère la liste de tous les fichiers dans le répertoire 'histoires'
-        files = os.listdir("histoires/")
+        # Récupère la liste de tous les fichiers dans le répertoire 'character'
+        files = os.listdir("character/")
         # Filtre la liste pour ne garder que les fichiers .txt
         txt_files = [f for f in files if f.endswith(".txt")]
         for filename in txt_files:
             filename_without_extension = os.path.splitext(filename)[0]
             print(filename_without_extension)
-        # Demander le titre de l'histoire à l'utilisateur
-        story_title = input("\nAvec quel personnage voulez-vous lancer le dialogue: ")
+        # Demander le nom du personnage à l'utilisateur
+        story_title = input("\nAvec quel personnage voulez-vous lancer le dialogue (saisir le nom en entier): ")
         duochat(story_title)
 
     elif choice == "5":
